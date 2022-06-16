3288071fe793b8fd33069b8b29cdb08f5b9d553d
kurinuts <120102kuriyama0921@gmail.com>
2022-06-16 10:48:31 +0000

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum age: { age20: 1, age30: 2,  age40: 3, age50: 4, age60: 5, age70: 6, age80: 7 }
-
 end
