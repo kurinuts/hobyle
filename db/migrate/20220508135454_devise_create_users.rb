0b11433a42be90b271285447acc4e1000b44c232
kurinuts <120102kuriyama0921@gmail.com>
2022-06-16 11:23:41 +0000

    fix

--- db/migrate/20220508135454_devise_create_users.rb
+++ db/migrate/20220508135454_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: "12345@docomo.ne.jp"
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -111,7 +111,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_081753) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "12345@docomo.ne.jp", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
